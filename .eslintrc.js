65957733403dffd08d15bd76b996c8f65b7504fd
baltazar119 <abdulgazisimsek119@gmail.com>
2025-06-27 14:56:17 +0300

    Disable no-explicit-any rule for Vercel

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// .eslintrc.js
+module.exports = {
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
